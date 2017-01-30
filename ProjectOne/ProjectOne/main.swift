3e2d5a8b7551456337290f3e80ca78b37cfa6c28
Joel Saltzman <talk@joelsaltzman.com>
2017-01-29 18:27:07 -0800

    Added example projects and workspace

---
+++ ProjectOne/ProjectOne/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectOne
+//
+//  Created by Joel Saltzman on 1/29/17.
+//  Copyright © 2017 Joel Saltzman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
