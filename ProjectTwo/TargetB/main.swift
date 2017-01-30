0217bafd17549eeb871a02217fa9b3a2d3455651
Joel Saltzman <talk@joelsaltzman.com>
2017-01-30 07:34:52 -0800

    Added TargetB

---
+++ ProjectTwo/TargetB/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TargetB
+//
+//  Created by Joel Saltzman on 1/30/17.
+//  Copyright © 2017 Joel Saltzman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
