71b76b961060bb0b13a1022f99efcf5bb39335b3
Joel Saltzman <talk@joelsaltzman.com>
2018-07-15 07:13:42 -0700

    Updated targets

--- ProjectTwo/KernelExtension/KernelExtension.c
+++ ProjectTwo/KernelExtension/KernelExtension.c
@@ -2,8 +2,8 @@
 //  KernelExtension.c
 //  KernelExtension
 //
-//  Created by Joel Saltzman on 2/12/17.
-//  Copyright © 2017 Joel Saltzman. All rights reserved.
+//  Created by Joel Saltzman on 7/15/18.
+//  Copyright © 2018 Joel Saltzman. All rights reserved.
 //
 
 #include <mach/mach_types.h>
